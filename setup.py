d716da84a479c7cb85e3d13ff7f846d31a55f18f
fatmamami <mamifatma8@gmail.com>
2022-12-04 19:51:54 +0100

    snowflake package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+
+    name="snowflake",
+    version="1",
+    author="DSSS",
+    author_email="fatma.mami@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy","turtle"]
+)
